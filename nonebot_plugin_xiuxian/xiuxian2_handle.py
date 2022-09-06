371a2be6877b355ccbf8b4baa7b1ebcbd5e2602d
yuzi <44226600+s52047qwas@users.noreply.github.com>
2022-09-06 13:53:56 +0800

    Update xiuxian2_handle.py

--- nonebot_plugin_xiuxian/xiuxian2_handle.py
+++ nonebot_plugin_xiuxian/xiuxian2_handle.py
@@ -66,7 +66,7 @@ class XiuxianDateManage:
                                     scheduled_time INTEGER);""")
 
         sql = "select * FROM level where name=?"
-        c.execute(sql, ("元婴境圆满",))
+        c.execute(sql, ("龙灵根",))
         result = c.fetchone()
         if result:
             pass
