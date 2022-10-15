463d81ecef175f91f849cb35427d9180245daed9
Shinoakii <wct151058@gmail.com>
2022-10-15 20:18:07 +0800

    调整boss数值

--- nonebot_plugin_xiuxian/xiuxian_boss/makeboss.py
+++ nonebot_plugin_xiuxian/xiuxian_boss/makeboss.py
@@ -5,6 +5,17 @@ from ..xiuxian2_handle import XiuxianDateManage
 
 DATEPATH = Path() / "data" / "xiuxian"
 jingjie = ['练气境', '筑基境', '结丹境', '元婴境', '化神境', '炼虚境', '合体境', '大乘境', '渡劫境']
+bossstone = {
+    '练气境':20000,
+    '筑基境':50000,
+    '结丹境':100000,
+    '元婴境':200000,
+    '化神境':300000,
+    '炼虚境':400000,
+    '合体境':500000,
+    '大乘境':600000,
+    '渡劫境':700000,
+}
 sql_message = XiuxianDateManage()  # sql类
 
 def createboss():
@@ -18,6 +29,7 @@ def createboss():
     bossnamelist = readf()
     bossinfo['name'] = random.choice(bossnamelist['Boss'])
     bossinfo['jj'] = nowjj
+    bossinfo['stone'] = bossstone[nowjj]
     return bossinfo
 
 
@@ -32,12 +44,9 @@ def get_boss_exp(alluserexp):
         bossexp += e[0]
     
     bossexp = int(bossexp / len(alluserexp))
-    print(bossexp)
-    bossinfo['气血'] = bossexp * 30
+    bossinfo['气血'] = bossexp * 15
     bossinfo['真元'] = bossexp * 10
-    bossinfo['攻击'] = bossexp / 10
-    bossinfo['stone'] = bossexp / 50
-    print(bossinfo)
+    bossinfo['攻击'] = int(bossexp / 5)
     return bossinfo
 
 BOSSDATA = DATEPATH / "boss.json"
