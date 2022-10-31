270df9be49d7585e2fe071061fcbb3f6ac90ce7d
Shinoakii <wct151058@gmail.com>
2022-11-01 00:00:39 +0800

    秘境todo

--- nonebot_plugin_xiuxian/xiuxian_rift/__init__.py
+++ nonebot_plugin_xiuxian/xiuxian_rift/__init__.py
@@ -16,66 +16,86 @@ from nonebot.log import logger
 from ..xiuxian2_handle import XiuxianDateManage
 import json
 from ..player_fight import Boss_fight
-from ..utils import data_check_conf
-from .jsondata import readgroup, savegroup
+from ..utils import data_check_conf, check_user
+from .jsondata import read_group, save_group
 
 # 定时任务
-setrift = require("nonebot_plugin_apscheduler").scheduler
-setgrouprift = on_command("群秘境", priority=5, permission= GROUP and (SUPERUSER | GROUP_ADMIN | GROUP_OWNER))
+set_rift = require("nonebot_plugin_apscheduler").scheduler
+set_group_rift = on_command("群秘境", priority=5, permission= GROUP and (SUPERUSER | GROUP_ADMIN | GROUP_OWNER))
+explore_rift = on_command("探索秘境", priority=5, permission= GROUP)
 
 __rift_help__ = f"""
 秘境帮助信息:
 指令：
 1、群秘境开启、关闭：开启本群的秘境生成任务，管理员权限
+2、探索秘境：探索秘境获取随机奖励
 非指令：
 
 """.strip()
 
-grouprift = {}
+group_rift = {}
 groups = {}
 try:
-    groups = readgroup()
+    groups = read_group()
 except:
     groups['open'] = []
     
 # 定时任务生成群秘境
-@setrift.scheduled_job("cron", hour=18, minute=30)
+@set_rift.scheduled_job("cron", hour=18, minute=30)
 async def _():
     bot = get_bot()
     if groups['open'] != []:
         for g in groups['open']:
-            grouprift[g]
+            group_rift[g]
             msg = f"已生成秘境"
             await bot.send_group_msg(group_id=int(g), message=msg)
 
+#探索秘境
+@explore_rift.handle()
+async def _(bot: Bot, event: GroupMessageEvent, args: Message = CommandArg()):
+    await data_check_conf(bot, event)
 
-@setgrouprift.handle()
+    isUser, user_info, msg = check_user(event)
+    if not isUser:
+        await explore_rift.finish(msg, at_sender=True)
+    group_id = event.group_id
+    if group_id not in groups['open']:
+        msg = '本群尚未开启秘境，请联系管理员开启群秘境'
+        await explore_rift.finish(msg, at_sender=True)
+    try:
+        group_rift[group_id]
+    except:
+        msg = '野外秘境尚未生成，请道友耐心等待!'
+        await explore_rift.finish(msg, at_sender=True)
+
+
+@set_group_rift.handle()
 async def _(bot: Bot, event: GroupMessageEvent, args: Message = CommandArg()):
     await data_check_conf(bot, event)
     mode = args.extract_plain_text().strip()
     group_id = event.group_id
-    isInGroup = isInGroups(event) #True在，False不在
+    is_in_group = is_in_groups(event) #True在，False不在
 
     if mode == '开启':
-        if isInGroup:
-            await setgrouprift.finish(f'本群已开启群秘境，请勿重复开启!')
+        if is_in_group:
+            await set_group_rift.finish(f'本群已开启群秘境，请勿重复开启!')
         else:
             groups['open'].append(group_id)
-            savegroup(groups)
-            await setgrouprift.finish(f'已开启本群秘境!')
+            save_group(groups)
+            await set_group_rift.finish(f'已开启本群秘境!')
 
     elif mode == '关闭':
-        if isInGroup:
+        if is_in_group:
             groups['open'].remove(group_id)
-            savegroup(groups)
-            await setgrouprift.finish(f'已关闭本群秘境!')
+            save_group(groups)
+            await set_group_rift.finish(f'已关闭本群秘境!')
         else:
-            await setgrouprift.finish(f'本群未开启群秘境!')
+            await set_group_rift.finish(f'本群未开启群秘境!')
     
     elif mode == '':
-        await setgrouprift.finish(__rift_help__)
+        await set_group_rift.finish(__rift_help__)
     else:
-        await setgrouprift.finish(f'请输入正确的指令：群秘境开启或关闭!')
+        await set_group_rift.finish(f'请输入正确的指令：群秘境开启或关闭!')
         
-def isInGroups(event: GroupMessageEvent):
+def is_in_groups(event: GroupMessageEvent):
     return event.group_id in groups['open']
--- nonebot_plugin_xiuxian/xiuxian_rift/jsondata.py
+++ nonebot_plugin_xiuxian/xiuxian_rift/jsondata.py
@@ -4,16 +4,16 @@ from pathlib import Path
 
 GROUPSJSONPATH = Path(__file__).parent
 FILEPATH = GROUPSJSONPATH / 'groups.json'
-def readgroup():
+def read_group():
     with open(FILEPATH, "r", encoding="UTF-8") as f:
         data = f.read()
     return json.loads(data)
 
 
-def savegroup(data):
+def save_group(data):
     data = json.dumps(data, ensure_ascii=False)
-    savemode = "w" if os.path.exists(FILEPATH) else "x"
-    with open(FILEPATH, mode=savemode, encoding="UTF-8") as f:
+    save_mode = "w" if os.path.exists(FILEPATH) else "x"
+    with open(FILEPATH, mode=save_mode, encoding="UTF-8") as f:
         f.write(data)
         f.close
     return True
