3f19e2f147f4fcae0a24fe5d14312f45fbe26fe0
wuyukwi <b1044763336@gmail.com>
2022-11-07 18:45:39 +0900

    修改秘境可进入次数

--- nonebot_plugin_xiuxian/xiuxian_rift/riftconfig.py
+++ nonebot_plugin_xiuxian/xiuxian_rift/riftconfig.py
@@ -9,54 +9,54 @@ CONFIG = {
         "东玄域": {
             "type_rate": 200,#概率
             "rank":1,#增幅等级
-            "count":3,#次数
+            "count":30,#次数
             "time":60,#时间，单位分
         },
         "西玄域": {
             "type_rate": 200,
             "rank":1,
-            "count":3,
+            "count":30,
             "time":60,
         },
         "妖域": {
             "type_rate": 100,
             "rank":2,
-            "count":5,
+            "count":50,
             "time":90,
         }, 
         "乱魔海": {
             "type_rate": 100,
             "rank":2,
-            "count":5,
+            "count":50,
             "time":90,
         },
         "幻雾林": {
             "type_rate": 50,
             "rank":3,
-            "count":5,
+            "count":50,
             "time":120,
         },
         "狐鸣山": {
             "type_rate": 50,
             "rank":3,
-            "count":5,
+            "count":50,
             "time":120,
         },
         "云梦泽": {
             "type_rate": 25,
             "rank":4,
-            "count":5,
+            "count":50,
             "time":150,
         },
         "乱星原": {
             "type_rate": 12,
-            "rank":4,
+            "rank":40,
             "count":5,
             "time":150,
         },
         "黑水湖": {
             "type_rate": 6,
-            "rank":5,
+            "rank":50,
             "count":5,
             "time":180,
         }
